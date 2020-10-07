939985d5d34bf1716108bcf268e939d7290faae4
Yen-An Jou <yjou@ncsu.edu>
2020-10-07 14:49:48 -0400

    remove uploader from submission model

--- app/models/submission_record.rb
+++ app/models/submission_record.rb
@@ -1,5 +1,5 @@
 class SubmissionRecord < ActiveRecord::Base
-  mount_uploader :content, SubmissionUploader # Tells rails to use this uploader for this model.
+  #mount_uploader :content, SubmissionUploader # Tells rails to use this uploader for this model.
   validates :content, presence: true
   validates :operation, presence: true
   validates :team_id, presence: true
